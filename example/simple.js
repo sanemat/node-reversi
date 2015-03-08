24c4be194db9f8f72804b14e968881d9c3046b15
sanemat <o.gata.ken@gmail.com>
2015-03-08 21:14:05 +0900

    chore(example): add examples

--- example/simple.js
+++ example/simple.js
@@ -9,5 +9,14 @@
 'use strict';
 
 var reversi = require('../');
+reversi.init(null, function(err, data){
+  data.turns;// []
+  data.matrix;// [ {"col": "a", "row": "1", "state": "empty"},
+              //  {"col": "b", "row": "1", "state": "empty"}, ... ]
+});
+reversi.set({"turns": [{"col": "b", "row": "1", "state": "black"}], "matrix": []}, function(err, data){
+  data.turns;
+  data.matrix;
+});
 
 console.log(reversi()); // "awesome"
