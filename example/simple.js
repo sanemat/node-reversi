19c7c0e8aed71efeaa9bc47b15498d5de76d01da
sanemat <o.gata.ken@gmail.com>
2015-03-09 21:03:56 +0900

    chore(reset): reset examples

--- example/simple.js
+++ example/simple.js
@@ -9,15 +9,3 @@
 'use strict';
 
 var reversi = require('../');
-reversi.init(4, function(err, data){
-  data.turns;// []
-  data.matrix;// [ {"col": "a", "row": "1", "state": "empty"},
-              //  {"col": "b", "row": "1", "state": "empty"}, ... ]
-});
-// matrix, turns, function
-reversi([], [{"col": "b", "row": "1", "state": "black"}], function(err, data){
-  data.turns;
-  data.matrix;
-});
-
-console.log(reversi()); // "awesome"
--- test/reversi_test.js
+++ test/reversi_test.js
@@ -4,25 +4,6 @@ var reversi = require('../');
 var assert = require('power-assert');
 
 describe('reversi', function () {
-  it('returns initial matrix', function () {
-    var expected = [
-      {'col': 'a', 'row': '1', 'state': 'empty'},
-      {'col': 'b', 'row': '1', 'state': 'empty'},
-      {'col': 'c', 'row': '1', 'state': 'empty'},
-      {'col': 'd', 'row': '1', 'state': 'empty'},
-      {'col': 'a', 'row': '2', 'state': 'empty'},
-      {'col': 'b', 'row': '2', 'state': 'white'},
-      {'col': 'c', 'row': '2', 'state': 'black'},
-      {'col': 'd', 'row': '2', 'state': 'empty'},
-      {'col': 'a', 'row': '3', 'state': 'empty'},
-      {'col': 'b', 'row': '3', 'state': 'black'},
-      {'col': 'c', 'row': '3', 'state': 'white'},
-      {'col': 'd', 'row': '3', 'state': 'empty'},
-      {'col': 'a', 'row': '4', 'state': 'empty'},
-      {'col': 'b', 'row': '4', 'state': 'empty'},
-      {'col': 'c', 'row': '4', 'state': 'empty'},
-      {'col': 'd', 'row': '4', 'state': 'empty'}
-    ];
-    assert.deepEqual(reversi.initialMatrix(4), expected);
+  it('returns', function () {
   });
 });
